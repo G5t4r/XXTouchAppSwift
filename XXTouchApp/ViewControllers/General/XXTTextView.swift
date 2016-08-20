523658a4971d065f07cfd43dc892376a34550176
苏泽 <havonz@me.com>
2016-08-20 10:25:12 +0800

    编辑器改成白底黑字，取消语法高亮先

--- XXTouchApp/ViewControllers/General/XXTTextView.swift
+++ XXTouchApp/ViewControllers/General/XXTTextView.swift
@@ -41,17 +41,19 @@ class XXTTextView: CYRTextView {
   }
   
   
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
   
-  private func setupUI() {
-    self.backgroundColor = UIColor(rgb: 0x272822)
-    self.textColor = UIColor.whiteColor()
-    self.font = defaultFont
-    self.autocorrectionType = .No // 关闭自动更正
-    self.tokens = solverTokens()
-  }
+    private func setupUI() {
+//        self.backgroundColor = UIColor(rgb: 0x272822)
+//        self.textColor = UIColor.whiteColor()
+        self.backgroundColor = UIColor.whiteColor()
+        self.textColor = UIColor.blackColor()
+        self.font = defaultFont
+        self.autocorrectionType = .No // 关闭自动更正
+        self.tokens = solverTokens()
+    }
   
   private func makeConstriants() {
     
@@ -92,60 +94,59 @@ class XXTTextView: CYRTextView {
   
   func solverTokens() -> [CYRToken] {
     let tokens: [CYRToken] = [
-      //      CYRToken(name: "special_numbers", expression: "[ʝ]", attributes: [NSForegroundColorAttributeName : UIColor.redColor()]),
-      
-      
-      
-      // 字符串
-      CYRToken(name: "string", expression: "\".*?(\"|$)", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xe7dc74)]),
-      
-      // 16进制
-      CYRToken(name: "hex", expression: "0x[0-9 a-f]+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 8进制
-      CYRToken(name: "octal", expression: "0o[0-7]+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 2进制
-      CYRToken(name: "binary", expression: "0b[01]+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 浮点
-      CYRToken(name: "float", expression: "\\d+\\.?\\d+e[\\+\\-]?\\d+|\\d+\\.\\d+|∞", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 整形
-      CYRToken(name: "integer", expression: "\\d+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 操作符
-      CYRToken(name: "operator", expression: "[/\\*,\\;:=<>\\+\\-\\^!·≤≥]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
-      
-      // 圆括号
-      //      CYRToken(name: "round_brackets", expression: "[\\(\\)]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
-      
-      // 方括号
-      //      CYRToken(name: "square_brackets", expression: "[\\[\\]]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
-      
-      // 花括号
-      CYRToken(name: "flower_brackets", expression: "[\\{\\}]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
-      
-      // 逻辑
-      //      CYRToken(name: "absolute_brackets", expression: "[|]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
-      
-      // 系统方法
-      CYRToken(name: "reserved_words", expression: "print|string.format|abs|string.char|math.pi|math.sin|string.upper|string.gsub|string.byte", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0x67daf0)]),
-      
-      // 关键词
-      CYRToken(name: "key_words", expression: "function|local|return|end|for|do|continue|goto", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
-      
-      // 参数
-      //      CYRToken(name: "chart_parameters", expression: "color|width", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xe2871d)]),
-      
-      
-      
-      // 注释
-      CYRToken(name: "comment", expression: "--.*", attributes: [
-        NSForegroundColorAttributeName : UIColor(rgb: 0x1f8300),
-        NSFontAttributeName : self.italicFont
-        ]
-      )
+//      //      CYRToken(name: "special_numbers", expression: "[ʝ]", attributes: [NSForegroundColorAttributeName : UIColor.redColor()]),
+//      
+//      
+//      
+//      // 字符串
+//        CYRToken(name: "string", expression: "\".*?(\"|$)", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xe7dc74)]),
+//        CYRToken(name: "string", expression: "\'.*?(\'|$)", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xe7dc74)]),
+//      
+//      // 16进制
+//      CYRToken(name: "hex", expression: "[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥](0x[0-9 a-f]+)[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
+//      
+////      // 8进制
+////      CYRToken(name: "octal", expression: "0o[0-7]+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
+////      
+////      // 2进制
+////      CYRToken(name: "binary", expression: "0b[01]+", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
+//      
+//      // 浮点
+//      CYRToken(name: "float", expression: "[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥]\\d+\\.?\\d+e[\\+\\-]?\\d+|\\d+\\.\\d+|∞[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
+//      
+//      // 整形
+//      CYRToken(name: "integer", expression: "[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥](\\d+)[\\s\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xae81ff)]),
+//      
+//      // 操作符
+//      CYRToken(name: "operator", expression: "[\\{\\}\\[\\]\\(\\)/\\*,\\;:=<>\\+\\-\\^!·≤≥]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//      // 圆括号
+//      //      CYRToken(name: "round_brackets", expression: "[\\(\\)]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//      // 方括号
+//            CYRToken(name: "square_brackets", expression: "[\\[\\]]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//        // 花括号
+//        CYRToken(name: "flower_brackets", expression: "[\\{\\}]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//      // 逻辑
+//      //      CYRToken(name: "absolute_brackets", expression: "[|]", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//      // 系统方法
+//      CYRToken(name: "reserved_words", expression: "print|string.format|abs|string.char|math.pi|math.sin|string.upper|string.gsub|string.byte", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0x67daf0)]),
+//      
+//      // 关键词
+//      CYRToken(name: "key_words", expression: "(function|local|return|end|for|do|continue|goto)", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xf92673)]),
+//      
+//      // 参数
+//      //      CYRToken(name: "chart_parameters", expression: "color|width", attributes: [NSForegroundColorAttributeName : UIColor(rgb: 0xe2871d)]),
+//      
+//        // 注释
+//        CYRToken(name: "comment", expression: "--.*", attributes: [
+//            NSForegroundColorAttributeName : UIColor(rgb: 0x1f8300),
+//            NSFontAttributeName : self.italicFont
+//            ]
+//        ),
       
     ]
     
